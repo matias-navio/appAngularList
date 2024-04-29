16659823857ab7e9bbcfc04b066ed9a3fae1ed53
Matias Navio <naviomatias2@gmail.com>
2024-04-29 18:17:04 -0300

    Estilos de las listas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductComponent } from "./pantallas/product/product.component";
+import { NavbarComponent } from "./shared/navbar/navbar.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, ProductComponent, NavbarComponent]
 })
 
 export class AppComponent {
 
-  
 }
--- src/app/shared/services/product.service.ts
+++ src/app/shared/services/product.service.ts
@@ -1,18 +1,17 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class ProductService {
 
-  productList: any[] = [];
+  listaProduct: any[] = [];
   private readonly _http = inject(HttpClient);
-
+  
   getProducts(): Observable<any[]>{
+
     return this._http.get<any[]>('https://fakestoreapi.com/products');
   }
-
 }
